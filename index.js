f70a7ff4278b4042a5e39beda5713a33084ac1e3
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2014-10-31 14:52:47 +0000

    Include match media on install

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  name: 'ember-cli-responsive'
+  name: 'ember-cli-responsive',
+  included: function (app) {
+    this._super.included(app);
+    app.import('bower_components/matchMedia/matchMedia.js');
+  }
 };
