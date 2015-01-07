b595452dbfcff880c2ff9f33630591358a7dfb64
Andrew Van Campenhout <Andrew.vanCampenhout@fourth.com>
2015-01-07 10:27:04 +0000

    Include matchMedia

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  name: 'ember-cli-responsive'
+  name: 'ember-cli-responsive',
+  included: function included(app) {
+    this.app = app;
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/matchMedia/matchMedia.js');
+  }
 };
